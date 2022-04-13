9ad9f8b82ae72ed4fd7b24df738ec6581e712078
GiangNguyenn <giang.nguyen@skedulo.com>
2022-04-13 16:27:29 +0700

    Add user redux actions and reducers

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,23 @@
+import { UserActionTypes } from '../constants/action-types';
+const intialState = {
+  users: [],
+};
+const { SET_USERS, SELECTED_USER } = UserActionTypes;
+export const usersReducer = (state = intialState, { type, payload }) => {
+  switch (type) {
+    case SET_USERS:
+      return { ...state, users: payload };
+    default:
+      return state;
+  }
+};
+
+export const selectedUserReducer = (state = {}, { type, payload }) => {
+  console.log(type);
+  switch (type) {
+    case SELECTED_USER:
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
+};
