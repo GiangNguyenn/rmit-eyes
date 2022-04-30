f372d41e3a8c4f2ed4fc7ee9f2411335e4505042
GiangNguyenn <giang.nguyen@skedulo.com>
2022-04-30 16:50:41 +0700

    Change BE base url to aws elastic beanstalk endpoint

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const devBaseUrl = 'http://localhost:3010';
+const devBaseUrl = 'http://rmiteyesbackend-env.eba-9bqmwkkv.us-west-2.elasticbeanstalk.com/';
 // Todo: Add production base url when deploy
 const productionBaseUrl = 'HerokuSomething';
 export default axios.create({
